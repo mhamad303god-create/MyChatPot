e07b14c8593b5f8b8cdb6362910615c78e98b5f7
Admin <admin@local>
2026-04-24 02:57:49 +0300

    إضافة رابط Render إلى ALLOWED_HOSTS

--- backend/settings.py
+++ backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-ptv^zmkxvk(oy$g68p%g177(8=)*-%m3yldpo%jfqs%3=uo7sw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mychatpot.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
